4a183caa5a2434cbea9bf224a195fb5348cb794e
Nguyen Truong <23653651.truong@student.iuh.edu.vn>
2025-09-30 11:11:28 +0700

    Giao dien login

--- src/main/java/iuh/fit/se/group13/Main.java
+++ src/main/java/iuh/fit/se/group13/Main.java
@@ -1,29 +1,17 @@
 package iuh.fit.se.group13;
 
+import iuh.fit.se.group13.view.Login_GUI;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button("Hello JavaFX with Maven!");
-        btn.setOnAction(e -> System.out.println("Button clicked!"));
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 400, 300);
-
-        primaryStage.setTitle("JavaFX Test App");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("Nguyễn Văn Trường");
+        new Login_GUI().setVisible(true);
+        ;
     }
 }
