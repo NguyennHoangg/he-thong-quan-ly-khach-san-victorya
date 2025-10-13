2f772d72979beddc46bdf702940579e7716e1b12
Cozy <139521084+lttboy25@users.noreply.github.com>
2025-10-13 09:04:08 +0700

    import them thu vien sua loi trang quan ly

--- src/main/java/view/TrangQuanLy.java
+++ src/main/java/view/TrangQuanLy.java
@@ -6,7 +6,9 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -17,6 +19,7 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
