bbd40ea84acd41e07baf753f2e897f80ec7858ab
lttboy25 <139521084+lttboy25@users.noreply.github.com>
2025-10-16 17:50:51 +0700

    Đẩy data lên table view ở DoiPhong_GUI

--- src/main/java/config/ConnectDatabase.java
+++ src/main/java/config/ConnectDatabase.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConnectDatabase {
 
-    private static final String JDBC_URL = "jdbc:sqlserver://localhost:14330;databaseName=Victorya_Hotel;encrypt=true;trustServerCertificate=true";
+    private static final String JDBC_URL = "jdbc:sqlserver://localhost:1433;databaseName=Victorya_Hotel;encrypt=true;trustServerCertificate=true";
     private static final String USER = "sa";
     private static final String PASSWORD = "sapassword";
 
