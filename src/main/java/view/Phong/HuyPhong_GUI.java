26b122cd3ae594bc8ca5e0d140d5c465ab32b4d6
NguyennHoangg <nguyenhuyhoang270804@gmail.com>
2025-12-11 12:59:28 +0700

    mupdate

--- src/main/java/view/Phong/HuyPhong_GUI.java
+++ src/main/java/view/Phong/HuyPhong_GUI.java
@@ -17,9 +17,9 @@ import model.ChiTietPhieuDatPhong;
 
 public class HuyPhong_GUI extends BorderPane {
     private TextField tfTimKiem;
-    private Button nutTimKiem;
+    private Button btnTimKiem;
     public TextArea txtLyDoHuyPhong;
-    private Button nutHuy;
+    private Button btnHuy;
 
     private VBox vboxDanhSachPhong;
     private ScrollPane cuonDanhSach;
@@ -36,6 +36,7 @@ public class HuyPhong_GUI extends BorderPane {
 
     private final Image anhThuong = new Image(getClass().getResource("/img/Thuong.jpg").toExternalForm());
     private final Image anhVip = new Image(getClass().getResource("/img/VIP.jpg").toExternalForm());
+    private Button btnLamMoi;
 
     public HuyPhong_GUI() {
         this.setPadding(new Insets(20));
@@ -73,11 +74,11 @@ public class HuyPhong_GUI extends BorderPane {
         tfTimKiem.setStyle(
                 "-fx-background-radius: 5; -fx-border-radius: 5; -fx-border-color: #d1d5db; -fx-background-color: white; -fx-padding: 0 15;");
 
-        nutTimKiem = new Button("Tìm kiếm");
-        nutTimKiem.setPrefHeight(40);
-        nutTimKiem.setPrefWidth(110);
-        nutTimKiem.getStyleClass().addAll("btn");
-        nutTimKiem.setOnAction(e -> {
+        btnTimKiem = new Button("Tìm kiếm");
+        btnTimKiem.setPrefHeight(40);
+        btnTimKiem.setPrefWidth(110);
+        btnTimKiem.getStyleClass().addAll("btn");
+        btnTimKiem.setOnAction(e -> {
             String maPhong = tfTimKiem.getText();
             hienThiPhong("Đã đặt", maPhong);
 
@@ -86,9 +87,17 @@ public class HuyPhong_GUI extends BorderPane {
             capNhatThongTinThanhToan();
         });
 
-        tfTimKiem.setOnAction(e -> nutTimKiem.fire());
+        btnLamMoi = new Button("🔄 Làm mới");
+        btnLamMoi.setPrefHeight(40);
+        btnLamMoi.setPrefWidth(110);
+        btnLamMoi.getStyleClass().addAll("btn");
+        btnLamMoi.setOnAction(e -> {
+            lamMoi();
+        });
 
-        hopTimKiem.getChildren().addAll(tfTimKiem, nutTimKiem);
+        tfTimKiem.setOnAction(e -> btnTimKiem.fire());
+
+        hopTimKiem.getChildren().addAll(tfTimKiem, btnTimKiem, btnLamMoi);
         hop.getChildren().add(hopTimKiem);
 
         return hop;
@@ -106,8 +115,9 @@ public class HuyPhong_GUI extends BorderPane {
 
         vboxDanhSachPhong.getChildren().add(tieuDe);
 
-        // Hiển thị lần đầu
-        hienThiPhong("Đã đặt", null);
+        // Hiển thị lần đầu không hiển thị để giảm tải RAM, không load hết database ngay
+        // lần đầu tiên
+        // hienThiPhong("Đã đặt", null);
 
         ScrollPane cuon = new ScrollPane(vboxDanhSachPhong);
         cuon.setFitToWidth(true);
@@ -264,11 +274,11 @@ public class HuyPhong_GUI extends BorderPane {
 
         chiTietBox.getChildren().addAll(hopTongTien, hopCoc, sep, hopTotal);
 
-        nutHuy = new Button("Hủy");
-        nutHuy.setPrefWidth(Double.MAX_VALUE);
-        nutHuy.setPrefHeight(45);
-        nutHuy.getStyleClass().add("btn-huy");
-        nutHuy.setOnAction(e -> {
+        btnHuy = new Button("Hủy");
+        btnHuy.setPrefWidth(Double.MAX_VALUE);
+        btnHuy.setPrefHeight(45);
+        btnHuy.getStyleClass().add("btn-huy");
+        btnHuy.setOnAction(e -> {
             if (!danhSachDaChon.isEmpty()) {
                 chiTietController.setDsPhongHuy(danhSachDaChon);
                 String lyDo = txtLyDoHuyPhong.getText();
@@ -284,9 +294,9 @@ public class HuyPhong_GUI extends BorderPane {
             }
         });
 
-        VBox.setMargin(nutHuy, new Insets(10, 0, 0, 0));
+        VBox.setMargin(btnHuy, new Insets(10, 0, 0, 0));
 
-        hop.getChildren().addAll(lblTieuDe, chiTietBox, nutHuy);
+        hop.getChildren().addAll(lblTieuDe, chiTietBox, btnHuy);
         return hop;
     }
 
